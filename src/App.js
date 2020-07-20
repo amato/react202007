6c99cd3bc9832a8a262b31db2cc4ab800ae85b48
phirip <phiriper@gmial.com>
2020-07-20 22:18:01 +0900

    divを変更React.Fragment

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
-import React ,{Component} from 'react';
+import React, {Component} from 'react';
 
 class App extends Component{
   render(){
-    return <div><h1>Hello,REACT!</h1></div>
+    return (
+      <React.Fragment>
+        <label htmlFor="bar">bar</label>
+        <input type="text" onChange={()=>{console.log("I am clicked!")}} />
+      </React.Fragment>
+    )
   }
 }
 
