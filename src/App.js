a20c725adf02d8e8382638b03f7c60a3a5fed2a9
phirip <phiriper@gmial.com>
2020-07-21 10:54:05 +0900

    state

--- src/App.js
+++ src/App.js
@@ -1,30 +1,40 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React,{Component} from 'react';
 
-const App = () => {
-  const profiles = [
-    {name:"Taro",age:10},
-    {name:"Hanako",age:4},
-    {name:"yagi",age:8}
-  ]
-  return (
-    <div>
-    {
-      profiles.map((profile,index)=>{
-        return <User name = {profile.name} age={profile.age} key={index} />
-      })
-    }
-    </div>
-  )
-}
+const App = () => (<Counter></Counter>)
 
-const User = (props) => {
-  return <div>Hi,I am {props.name},and {props.age}!</div>
-}
+class Counter extends Component{
+  constructor(props){//初期化処理
+    super(props)
+    console.log("constructor",this.state)
+    this.state = {count:0}
 
-User.propTypes = {
-  name: PropTypes.string,
-  age:PropTypes.number.isRequired
+  }
+
+  handlePlusButton = () =>{
+    const count = this.state.count
+    this.setState({count:count+1})
+
+    console.log("handlePlusButton:")
+    console.log(this.state.count)
+  }
+  handleMinusButton = () =>{
+    const count = this.state.count
+    this.setState({count:count-1})
+
+    console.log("handleMinusButton:")
+    console.log(this.state.count)
+  }
+
+  render(){
+    console.log("render",this.state)
+    return (
+      <React.Fragment>
+        <div>count: {this.state.count}</div>
+        <button onClick={this.handlePlusButton}>+1</button>
+        <button onClick={this.handleMinusButton}>-1</button>
+      </React.Fragment>
+    )
+  }
 }
 
 export default App;
---
+++ src/Appのコピー.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const App = () => {
+  const profiles = [
+    {name:"Taro",age:10},
+    {name:"Hanako",age:4},
+    {name:"yagi",age:8}
+  ]
+  return (
+    <div>
+    {
+      profiles.map((profile,index)=>{
+        return <User name = {profile.name} age={profile.age} key={index} />
+      })
+    }
+    </div>
+  )
+}
+
+const User = (props) => {
+  return <div>Hi,I am {props.name},and {props.age}!</div>
+}
+
+User.propTypes = {
+  name: PropTypes.string,
+  age:PropTypes.number.isRequired
+}
+
+export default App;
